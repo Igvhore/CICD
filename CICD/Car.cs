12f3355ae08ff1f0d77c5f30d3e01315d6746eb2
Игорь Алиев <aliev.igor1@mail.ru>
2024-10-23 22:26:53 +0300

    Добавлены комментарии

--- CICD/Car.cs
+++ CICD/Car.cs
@@ -6,12 +6,20 @@ using System.Threading.Tasks;
 
 namespace CICD
 {
+    /// <summary>
+    /// Это Класс для работы с Автомобилями
+    /// </summary>
+
     public class Car
     {
         public int id;
         public string name ="";
         public string description = "";
 
+        /// <summary>
+        /// Это метод, который проверяет ID машины.
+        /// </summary>
+ 
         public bool CheckID()
         {
             if (this.id <= 0)
@@ -26,11 +34,20 @@ namespace CICD
             }
         }
 
+        /// <summary>
+        /// Это метод, который позволяет дать машине имя.
+        /// </summary>
+
+
         public void SetCarName (string name)
         { 
             this.name = name;
             Console.WriteLine($"New name: {this.name}");
         }
+
+        /// <summary>
+        /// Это метод, который позволяет дать машине описание.
+        /// </summary>
 
         public void SetCarDescription(string description)
         {
